ec6c518367baa9b6fc5ffea792a59413539a1c40
Pele <pele_alex02@yahoo.com>
2022-04-11 12:42:20 +0300

    auto push 13

--- TPp1/TPp1/problema1.c
+++ TPp1/TPp1/problema1.c
@@ -4,15 +4,16 @@
 
 int main()
 {
-	int n, rez, put, rest;
+	int n, rez=0, put=1, rest;
 	printf("Dati numarul dorit: ");
 	scanf("%d", &n);
 	while (n != 0)
 	{
 		rest = n % 2;
 		rez = rez + rest * put;
-		nr = nr / 2;
+		put = put * 10;
+		n = n / 2;
 	}
-
+	printf("%d", rez);
 	return 0;
 }
