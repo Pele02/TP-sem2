60bbc315c94df81d8c912edef41dc80d79108082
Pele <pele_alex02@yahoo.com>
2022-04-11 12:34:43 +0300

    auto push 10

--- TPp1/TPp1/problema1.c
+++ TPp1/TPp1/problema1.c
@@ -4,16 +4,20 @@
 
 int main()
 {
-	int n, i, k;
+	int n, i, k, nr[34],cnt;
 	printf("Dati numarul dorit: ");
 	scanf("%d", &n);
 	for (i = 31; i >= 0; i--)
 	{
 		k = n >> i;
 		if (k & 1)
-			printf("1");
+		{
+			nr[cnt] = '1';
+			cnt++;
+		}
 		else
-			printf("0");
+			nr[cnt] = '0';
+		cnt
 	}
 
 	return 0;
