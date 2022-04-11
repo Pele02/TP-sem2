c88a69131eb8e79152f668a82d261b8374fe345e
Pele <pele_alex02@yahoo.com>
2022-04-11 12:22:02 +0300

    auto push 5

--- TPp1/TPp1/problema1.c
+++ TPp1/TPp1/problema1.c
@@ -1,8 +1,11 @@
-	for (i = 31; i >= 0; i--)
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
 {
-	k = nr >> i;
-	if (k & 1)
-	printf("1");
-	else
-	printf("0");
- }
+	int n, i;
+	printf("Dati numarul dorit: ");
+	scanf("%d", &n);
+	printf("%d", n);
+}
